00fb230b983edcbb7dc37be2903d5d095825c812
Stanley Chow <sjchow97@gmail.com>
2022-09-05 17:21:42 -0700

    fixed checkstyle errors

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,9 @@
 package org.comp2522;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("My favourite animals are cats!");
-    }
+/**
+ * Main.java was created to explore the Intellij IDE and push into a remote repository.
+ */
+public class Main { public static void main(String[] args) {
+    System.out.println("My favourite animals are cats!");
+  }
 }
