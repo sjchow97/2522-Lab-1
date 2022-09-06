4f9efcef4304c595b2adc26385b6083a86c5944d
Stanley Chow <sjchow97@gmail.com>
2022-09-06 10:32:17 -0700

    added additional documentation

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -2,6 +2,9 @@ package org.comp2522;
 
 /**
  * Main.java was created to explore the Intellij IDE and push into a remote repository.
+ *
+ * @author Stanley Chow
+ * @version 1.0
  */
 public class Main { public static void main(String[] args) {
     System.out.println("My favourite animals are cats!");
